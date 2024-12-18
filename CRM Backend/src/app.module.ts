9232c5e9661a037a56467a5586800c19fb17c1c3
Manish07122001Mahto <manishmahto.8885@gmail.com>
2024-12-18 13:09:23 +0530

    updated

--- CRM Backend/src/app.module.ts
+++ CRM Backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { envConfig } from './config/env.config';
 import { databaseConfig } from './config/database.config';
@@ -8,15 +6,16 @@ import { ContactModule } from './contacts/contact.module';
 import { ActivityModule } from './activity/activity.module';
 import { DealModule } from './deal/deal.module';
 
+
 @Module({
   imports: [
     envConfig, // Use the envConfig to load .env variables globally
     TypeOrmModule.forRoot(databaseConfig),
     ContactModule,
     ActivityModule,
-    DealModule, // Set up TypeORM with the configuration from database.config.ts
+    DealModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
